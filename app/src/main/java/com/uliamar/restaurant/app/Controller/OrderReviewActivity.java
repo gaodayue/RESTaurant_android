bd05544cd42c20ed68f52a5a00cf60fdaed31dde
legarspol <le-gar_p@epitech.eu>
2014-05-15 15:38:38 +0800

    Add a navigation button

--- app/src/main/java/com/uliamar/restaurant/app/Controller/OrderReviewActivity.java
+++ app/src/main/java/com/uliamar/restaurant/app/Controller/OrderReviewActivity.java
@@ -3,10 +3,12 @@ package com.uliamar.restaurant.app.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.uliamar.restaurant.app.R;
@@ -14,7 +16,8 @@ import com.uliamar.restaurant.app.R;
 public class OrderReviewActivity extends ActionBarActivity {
     TextView mCountDownTextView;
     public static final String ARG_INVITATION_ID = "ARG_INVITATION_ID";
-
+    private TextView addressTextView;
+    Context mContext;
 
     public static Intent createIntent(Context c, int invitationID) {
         Intent myIntent = new Intent(c, OrderReviewActivity.class);
@@ -25,10 +28,19 @@ public class OrderReviewActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_order_review);
         mCountDownTextView = (TextView) findViewById(R.id.CountDown);
         Typeface fontRobo = Typeface.createFromAsset(this.getAssets(), "fonts/roboto_thin.ttf");
         mCountDownTextView.setTypeface(fontRobo);
+        addressTextView = (TextView) findViewById(R.id.address);
+        addressTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=New+York+NY"));
+                mContext.startActivity(i);
+            }
+        });
     }
 
 
